950473d8b53210a700680a7b00b1c15483750156
Eddie Cervello <eddie@cervello.me>
2025-07-07 03:59:35 +0000

    feat: add PowerShell 7+ version check and user guidance for upgrade; robust error message for unsupported shells

--- RenameFiles.ps1
+++ RenameFiles.ps1
@@ -1,4 +1,10 @@
-#requires -Version 7.0
+#region PowerShell 7+ version check
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Error "This script requires PowerShell 7.0 or higher. Please install PowerShell 7+ from https://aka.ms/pwsh and run with 'pwsh'."
+    exit 1
+}
+#endregion
+
 Import-Module "$PSScriptRoot/RenameFiles/RenameFiles.psm1" -Force
 
 [CmdletBinding()]
